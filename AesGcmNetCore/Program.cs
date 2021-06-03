fdab1838f9704dffe7d4b13e02753f4aeaf0a4b1
Sergey .NET <sergeydotnet@outlook.com>
2021-06-03 17:48:52 +0200

    Created

---
+++ AesGcmNetCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AesGcmNetCore
+{
+    public class Program
+    {        
+        public static Task Main()
+        {
+            Console.WriteLine("Hello World!");
+            return Task.CompletedTask;
+        }
+    }
+}
